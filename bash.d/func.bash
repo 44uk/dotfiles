e523295d423d58b76f8472c0634d831d392c2a6e
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-05-04 02:07:53 +0900

    update

--- bash.d/func.bash
+++ bash.d/func.bash
@@ -7,3 +7,11 @@ function docker-rmi-none {
   docker images | awk '/<none/{print $3}' | xargs docker rmi
 }
 alias dkrmi='docker-rmi-none'
+
+function pyserv {
+  python -m http.server 8100
+}
+
+function rbserv {
+  ruby -rwebrick -e 'WEBrick::HTTPServer.new(:DocumentRoot => "./", :Port => 8100).start'
+}
--- scripts/rbenv_setup.sh
+++ scripts/rbenv_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ ! -e ~/.rbenv ]; then
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-  # add plugins(ruby-build)
-  mkdir -p ~/.rbenv/plugins
-  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  mkdir -p "$(rbenv root)"/plugins
+  git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 fi
